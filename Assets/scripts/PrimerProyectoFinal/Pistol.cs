3a0fc1790d07803d6972e5b98ea49afae88d878b
Jonathan Garnica <jony_carp_01@hotmail.com>
2023-02-17 00:18:44 -0300

    agregado de Switch para el arma

--- Assets/scripts/PrimerProyectoFinal/Pistol.cs
+++ Assets/scripts/PrimerProyectoFinal/Pistol.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum Change
+{
+    speedPistol,
+    gunWithTime
+}
 public class Pistol : MonoBehaviour
 {
     //[SerializeField] private GameObject Bullet_Pistol;
     [SerializeField] private Bullet_Pistol bullet_Pistol;
     [SerializeField] private Transform empty;
+    [SerializeField] private Change speedchange;
     [SerializeField] private float shotForce;
     [SerializeField] private float shotRate;
 
@@ -21,15 +28,7 @@ public class Pistol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (Time.time>shotRateTime)
-            {
-                Shoot();
-
-                shotRateTime = Time.time + shotRate;
-            }
-        }
+        Switch();
     }
 
     private void Shoot()
@@ -37,9 +36,38 @@ public class Pistol : MonoBehaviour
         var newPistol = Instantiate(bullet_Pistol, empty.position,empty.rotation);
         newPistol.GetComponent<Rigidbody>().AddForce(empty.forward * shotForce, ForceMode.Impulse);
 
+    }
 
-       
-
-
+    private void Switch()
+    {
+        switch (speedchange)
+        {
+            case Change.speedPistol:
+                VelocityChange();
+                break;
+            case Change.gunWithTime:
+                GunTime();
+                break;
+        }
+    }
+    private void GunTime()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time > shotRateTime)
+            {
+                Shoot();
+                shotRateTime = Time.time + shotRate;
+            }
+        }
+            
+    }
+    private void VelocityChange()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+            
     }
 }
